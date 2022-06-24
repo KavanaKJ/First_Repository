32db7270d14751c1557b5c47cf32bcb2858bb14d
KavanaKJ <101441423+KavanaKJ@users.noreply.github.com>
2022-06-24 11:40:08 +0530

    Update main.cpp

--- TestW/main.cpp
+++ TestW/main.cpp
@@ -13,15 +13,15 @@ int main(int argc, char *argv[])
     QPushButton *qpb1=new QPushButton("PushButton",&w);
     QCheckBox *qcb=new QCheckBox("checkbox",&w);
     QRadioButton *qrb=new QRadioButton("Radiobutton",&w);
-//   qpb->setGeometry(10,10,100,100);
-//   qpb1->setGeometry(120,10,100,100);
-//   qcb->setGeometry(230,10,100,100);
-//   qrb->setGeometry(320,10,100,100);
+  qpb->setGeometry(10,10,100,100);
+  qpb1->setGeometry(120,10,100,100);
+  qcb->setGeometry(230,10,100,100);
+  qrb->setGeometry(320,10,100,100);
 
-   qpb->setGeometry(0,0,100,100);
-   qpb1->setGeometry(100,100,100,100);
-   qcb->setGeometry(200,180,100,100);
-   qrb->setGeometry(260,260,100,100);
+//    qpb->setGeometry(0,0,100,100);
+//    qpb1->setGeometry(100,100,100,100);
+//    qcb->setGeometry(200,180,100,100);
+//    qrb->setGeometry(260,260,100,100);
     w.setGeometry(200,100,100,100);
     w.resize(500,500);
     w.show();
